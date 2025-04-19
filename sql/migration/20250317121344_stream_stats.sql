de9742e30e55ed0e73ab556de755b63a8fb75f52
Chimder <dimas.kirilyuk@gmail.com>
2025-04-19 13:51:00 +0300

    add upper sql

--- sql/migration/20250317121344_stream_stats.sql
+++ sql/migration/20250317121344_stream_stats.sql
@@ -1,27 +1,27 @@
 -- +goose Up
 -- +goose StatementBegin
-create extension if not exists "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-create table stream_stats (
-    id uuid primary key default uuid_generate_v4(),
-    stream_id varchar(255) not null,
-    user_id varchar(255) not null,
-    game_id varchar(255) not null,
-    date date not null,
-    airtime int default 0,
-    peak_viewers int default 0,
-    average_viewers int default 0,
-    hours_watched int default 0,
-    unique (stream_id, date)
+CREATE TABLE stream_stats (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    stream_id varchar(255) NOT NULL,
+    user_id varchar(255) NOT NULL,
+    game_id varchar(255) NOT NULL,
+    date date NOT NULL,
+    airtime int DEFAULT 0,
+    peak_viewers int DEFAULT 0,
+    average_viewers int DEFAULT 0,
+    hours_watched int DEFAULT 0,
+    UNIQUE (stream_id, date)
 );
 
-create index idx_stream_id on stream_stats (stream_id);
-create index idx_user_id on stream_stats (user_id);
+CREATE INDEX idx_stream_id ON stream_stats (stream_id);
+CREATE INDEX idx_user_id ON stream_stats (user_id);
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-drop table if exists stream_stats;
+DROP TABLE IF EXISTS stream_stats;
 
 -- +goose StatementEnd
